c7b6fd94c74e49b2f9259f1ce2efd11b77e9eb88
goranbrostrom <goran.brostrom@umu.se>
2021-01-16 15:25:03 +0100

    polishing updates

--- R/fit.out.R
+++ R/fit.out.R
@@ -1,11 +1,15 @@
 fit.out <- function(fit, caption, label){
     ## Output of regression results
     if (knitr::is_latex_output()){ # PDF
+        if (!missing(label)){
+            label <- paste0("tab:", label)
+        }
         dr <- drop1(fit, test = "Chisq")
-        ltx(fit, dr = dr, caption = caption, label = "tab:coxold8")
+        ltx(fit, dr = dr, caption = caption, label = label)
     }else{ # HTML
-        xx <- summary(fit)$coefficients[, 1:4]
-        kbl(round(xx, 4), booktabs = TRUE, caption = caption) %>%
-            kable_styling(font_size = 12, full_width = FALSE)
+        xx <- regtable(summary(fit), digits = 4)
+        kbl(xx, booktabs = TRUE, caption = caption) %>%
+            kable_styling(##font_size = 12, 
+                full_width = FALSE)
     }
 }
--- R/tbl.R
+++ R/tbl.R
@@ -1,6 +1,6 @@
 tbl <- function(tt, caption = ""){
     library(kableExtra)
-    kbl(tt, caption = caption, booktabs = TRUE) %>%
+    kbl(tt, caption = caption, booktabs = TRUE, row.names = FALSE) %>%
         kable_styling(full_width = FALSE, 
                     position = "center")
 }
