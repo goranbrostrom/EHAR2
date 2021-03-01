2b2f723b85f47ddb44e1bc99065e2f12cc4f8479
goranbrostrom <goran.brostrom@umu.se>
2021-03-01 12:58:04 +0100

    Keeping up-to-date

--- R/tbl.R
+++ R/tbl.R
@@ -1,6 +1,6 @@
-tbl <- function(tt, caption = ""){
+tbl <- function(tt, caption = "", fs = 11){
     library(kableExtra)
     kbl(tt, caption = caption, booktabs = TRUE, row.names = FALSE) %>%
-        kable_styling(full_width = FALSE, 
+        kable_styling(full_width = FALSE, font_size = fs,
                     position = "center")
 }
