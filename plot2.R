32be521f065cb2a33fd132e81681bfafed72ebae
goranbrostrom <goran.brostrom@umu.se>
2021-08-17 00:05:39 +0200

    better scatterplot

--- plot2.R
+++ plot2.R
@@ -1,8 +1,10 @@
 plot2 <- function(){
-    x <- rnorm(100)
-    y <- x + rnorm(100, sd = 0.1)
+    set.seed(1211) # fix random sequence.
+    x <- 3 + rnorm(100)
+    y <- 2 + x + rnorm(100, sd = 0.5)
     oldpar <- par(mfrow = c(1, 2))
     on.exit(par(oldpar))
+    par(las = 1)
     plot(x, y, main = "Scatterplot of x and y")
     hist(y, main = "Histogram of y", probability = TRUE)
 }
